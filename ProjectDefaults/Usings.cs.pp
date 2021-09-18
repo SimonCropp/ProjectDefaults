116d34c804f935c5bcda6bb051b8cf9ad3353251
Simon Cropp <simon.cropp@gmail.com>
2021-09-18 19:07:57 +1000

    more usings

--- ProjectDefaults/Usings.cs.pp
+++ ProjectDefaults/Usings.cs.pp
@@ -3,3 +3,7 @@ global using System.Collections.Generic;
 global using System.IO;
 global using System.Text;
 global using System.Threading.Tasks;
+global using System.Linq;
+global using System.Diagnostics;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
